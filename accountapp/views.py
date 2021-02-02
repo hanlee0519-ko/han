a28816dff555d01d8b693017d5b0bd4ef4350969
han <han@hanui-MacBookPro.local>
2021-02-03 06:06:05 +0900

    django course 24 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -30,3 +30,9 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world') # class 사용 시에는 reverse_lazy
     template_name = 'accountapp/create.html' # class 4줄 만으로, 위에 있는 함수형과 차이
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'  # A가 B의 화면을 제대로 보기 위해
+    template_name = 'accountapp/detail.html'

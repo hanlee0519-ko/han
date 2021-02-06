1a9e049f4a670d0bb45ded1e7a2851d839cceb9a
han <han@hanui-MacBookPro.local>
2021-02-06 12:22:54 +0900

    django course 32 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -20,3 +22,13 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user # 서버 내에서 구현 / 클라이언트에서 유저를 정해서 구현하게 되면, 잘못하게 만들 수 있기에
         temp_profile.save()
         return super().form_valid(form)
+
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'

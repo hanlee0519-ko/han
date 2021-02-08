a9d4b7dccb6d49d05693176af5b93113322f69ff
han <han@hanui-MacBookPro.local>
2021-02-09 00:16:02 +0900

    django course 41 commit

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -6,4 +6,4 @@ from articleapp.models import Article
 class ArticleCreationForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'image', 'content'] # writer는 유저와 마찬가지로 서버에서, view에서 하겠구먼
+        fields = ['title', 'image', 'project', 'content'] # writer는 유저와 마찬가지로 서버에서, view에서 하겠구먼

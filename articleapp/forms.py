e0d9043cdc281b8b7fc646eafeb1ee2a1c55277a
han <han@hanui-MacBookPro.local>
2021-02-09 13:19:21 +0900

    django course 44 commit

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-align: left',
+                                                           'style': 'height: auto;'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
-        fields = ['title', 'image', 'project', 'content'] # writer는 유저와 마찬가지로 서버에서, view에서 하겠구먼
+        fields = ['title', 'image', 'project', 'content']
